804df4f6ea5d9836e04a0f645a9da81f50361a64
techavi <tech.avi.co@gmail.com>
2024-02-19 03:45:11 +0530

    my initial commit

--- app.cjs
+++ app.cjs
@@ -1,9 +1,10 @@
 
 var express = require('express');
+var axios = require('axios');
 const bodyParser = require('body-parser');
 var app = express();
-var port = 4000;
-const fictionalCats = require('./cats');
+var port = 4001;
+const fictionalCats = require('./cats.cjs');
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -11,7 +12,24 @@ app.use(express.json());
 // app.use(bodyParser.json);
 // app.use(bodyParser.urlencoded({extended:false}));
 
+app.get('/myPage',(req,res)=>{
+   //  res.send('Hello World');
 
+     axios
+     .get(
+         `http://192.168.29.190:4000/My_Api/users`
+     )
+     .then((response) => {
+         const posts = response.data;
+
+         console.log(">>>>>>",posts[0].displayName);
+        
+        res.send(`<h1>${posts[0].displayName}</h1>`);
+        // this.setState({ posts });
+     });
+
+     });
+    
 
 app.post('/api/post',(req,res)=>{
    const newCat = {
@@ -29,13 +47,16 @@ app.post('/api/post',(req,res)=>{
 app.get("/user/api",(req,res)=>{
 
    res.send([
-      { id:1,username:"aaa",displayName:"aaa"},
+      { id:1,username:"aaa",displayName:"How are you........"},
       { id:2,username:"bbb",displayName:"bbb"},
       { id:3,username:"ccc",displayName:"ccc"},
 
    ]);
 
 })
+
+
+
 
 app.get('/cat',(req,res)=>{
 res.send(`<h1>Fictional Cats</h1>
@@ -55,7 +76,7 @@ app.get('/form',(req,res)=>{
          });
 
          app.get('/',(req,res)=>{
-            res.sendFile(__dirname + "/Myhover.html");
+            res.sendFile(__dirname + "/index.html");
             });
 
 app.listen(port,'0.0.0.0',function(){
